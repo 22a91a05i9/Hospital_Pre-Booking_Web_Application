f0b4a07bca93846c504aa8063d55ce8de173e520
Lokanadham Jyoshnavi <22A91A05I9@aec.edu.in>
2025-09-26 13:23:39 +0530

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router} from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
